fbf715f7788f25065285612e0dab189a7787cb54
neung <manassiyaporn.amorn@gmail.com>
2020-06-26 18:48:01 +0700

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
 
 const mockUserData = [
     {
@@ -28,6 +31,29 @@ app.get(
             message: 'got one user',
             user: req.params.id
         })
+    }
+)
+
+app.post(
+    '/login', (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        const mockUsername = "billyTheKid";
+        const mockPassword = "superSecret";
+
+        if (username === mockUsername && password === mockPassword) {
+            res.json({
+                success: true,
+                message: 'password and username match!',
+                token: 'encrypted token goes here'
+            })
+        } else {
+            res.json({
+                success: false,
+                message: 'password and username do not match'
+            })
+        }
     }
 )
 
